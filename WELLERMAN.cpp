024d316a8c1cdfa473bc739186e4e5e3d0e5f44b
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-06-16 21:23:28 +0300

    Update WELLERMAN.cpp

--- WELLERMAN.cpp
+++ WELLERMAN.cpp
@@ -1,26 +1,26 @@
 #include<iostream>
-#include<windows.h>			// for seting the screen colour to blue
+#include<windows.h>// for seting the screen colour to blue
 using namespace std;
 
-void magazien() {			// output all that
+void magazien() {// output all that
 	cout << "\n\tWELLERMAN \t SONG" << endl;
 	cout << "> please chose which page that you wanna read ^_^ " << endl;
 	cout << "> 9 Pages(0 will show all of them)" << endl;
 	cout << "> Press q to Quit\n" << endl;
 }
-void p1() {					// output all that
+void p1() {// output all that
 	cout << " There once was a ship that put to sea " << endl;
 	cout << " The name of the ship was the Billy of Tea " << endl;
 	cout << " The winds blew up, her bow dipped down " << endl;
 	cout << " O blow, my bully boys, blow (huh)\n " << endl;
 }
-void p2() {					// output all that
+void p2() {// output all that
 	cout << " She had not been two weeks from shore " << endl;
 	cout << " When down on her a right whale bore " << endl;
 	cout << " The captain called all hands and swore " << endl;
 	cout << " He'd take that whale in tow (huh)\n " << endl;
 }
-void p3() {					// output all that
+void p3() {// output all that
 	cout << " Soon may the Wellerman come " << endl;
 	cout << " To bring us sugar and tea and rum (hey) " << endl;
 	cout << " One day, when the tonguin' is done " << endl;
