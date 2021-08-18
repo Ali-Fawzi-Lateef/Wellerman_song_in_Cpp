375c02bdfeef15aa9799b5b7d1d3b7cae53239ca
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-08-18 20:17:18 +0300

    Update WELLERMAN.cpp

--- WELLERMAN.cpp
+++ WELLERMAN.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
-#include<windows.h>// for seting the screen colour to blue.
+#include<windows.h>// for seting the screen colour to blue
 using namespace std;
 
-void magazien() {// output all that.
+void magazien() {// output all that
 	cout << "\n\tWELLERMAN \t SONG" << endl;
 	cout << "> please chose which page that you wanna read ^_^ " << endl;
 	cout << "> 9 Pages(0 will show all of them)" << endl;
 	cout << "> Press q to Quit\n" << endl;
 }
-void p1() {// output all that.
+void p1() {// output all that
 	cout << " There once was a ship that put to sea\a " << endl;
 	cout << " The name of the ship was the Billy of Tea " << endl;
 	cout << " The winds blew up, her bow dipped down " << endl;
 	cout << " O blow, my bully boys, blow (huh)\n " << endl;
 }
-void p2() {// output all that.
+void p2() {// output all that
 	cout << " She had not been two weeks from shore\a " << endl;
 	cout << " When down on her a right whale bore " << endl;
 	cout << " The captain called all hands and swore " << endl;
@@ -117,9 +117,9 @@ int main() {
 			cout << "Goodbye!";
 			break;
 		default:
-			cout << "invalid option >_< ";//if the user has inputed wrong option.
+			cout << "invalid option >_< ";//if the user has inputed wrong option
 			break;
 
 		}
-	} while (x != 'q');//break the loop when user enter q.
+	} while (x != 'q');//break the loop when user enter q
 }
